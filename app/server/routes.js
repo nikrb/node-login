0ca5603db136439b8f449d56f37d316e36870e8a
nik <nik_rb@yahoo.com>
2016-04-06 18:21:29 +0000

    group routing

--- app/server/routes.js
+++ app/server/routes.js
@@ -62,14 +62,14 @@ module.exports = function(app) {
 		}
 	});
 
-// main login page //
-	app.get('/', function(req, res){
-	// check if the user's credentials are saved in a cookie //
+	// json main login page //
+	app.get('/gollum', function(req, res){
+		// check if the user's credentials are saved in a cookie //
 		if (req.cookies.user == undefined || req.cookies.pass == undefined){
 			// res.render('login', { title: 'Hello - Please Login To Your Account' });
 			res.send( [{login:false}]);
 		}	else{
-	// attempt automatic login //
+		// attempt automatic login //
 			AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
 				if (o != null){
 				    req.session.user = o;
@@ -84,7 +84,7 @@ module.exports = function(app) {
 		}
 	});
 	
-	app.post('/', function(req, res){
+	app.post('/gollum', function(req, res){
 		AM.manualLogin(req.body['user'], req.body['pass'], function(e, o){
 			if (!o){
 				console.log( "login failed for [%s]", req.body['user']);
@@ -102,7 +102,7 @@ module.exports = function(app) {
 	});
 	
 
-	app.post('/logout', function(req, res){
+	app.post('/gollumout', function(req, res){
 		console.log( "post /logout:");
 		res.clearCookie('user');
 		res.clearCookie('pass');
@@ -111,7 +111,7 @@ module.exports = function(app) {
 	
 // creating new accounts //
 	
-	app.post('/signup', function(req, res){
+	app.post('/gollumsignup', function(req, res){
 		AM.addNewAccount({
 			name 	: req.body['name'],
 			email 	: req.body['email'],
@@ -129,59 +129,109 @@ module.exports = function(app) {
 		});
 	});
 
-// password reset //
 
-	app.post('/lost-password', function(req, res){
-	// look up the user's account via their email //
-		AM.getAccountByEmail(req.body['email'], function(o){
-			if (o){
-				EM.dispatchResetPasswordLink(o, function(e, m){
-				// this callback takes a moment to return //
-				// TODO add an ajax loader to give user feedback //
-					if (!e){
-						res.status(200).send('ok');
-					}	else{
-						for ( var k in e) console.log('ERROR : ', k, e[k]);
-						res.status(400).send('unable to dispatch password reset');
+	//**************************************************************************** browser site
+	
+	app.get('/', function(req, res){
+		// check if the user's credentials are saved in a cookie //
+		if (req.cookies.user == undefined || req.cookies.pass == undefined){
+			res.render('login', { title: 'Hello - Please Login To Your Account' });
+		}	else{
+		// attempt automatic login //
+			AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+				if (o != null){
+				    req.session.user = o;
+					res.redirect('/home');
+				}	else{
+					res.render('login', { title: 'Hello - Please Login To Your Account' });
+				}
+			});
+		}
+	});
+	
+	app.post('/', function(req, res){
+		AM.manualLogin(req.body['user'], req.body['pass'], function(e, o){
+			if (!o){
+				res.status(400).send(e);
+			}	else{
+				req.session.user = o;
+				if (req.body['remember-me'] == 'true'){
+					res.cookie('user', o.user, { maxAge: 900000 });
+					res.cookie('pass', o.pass, { maxAge: 900000 });
+				}
+				res.status(200).send(o);
+			}
+		});
+	});
+
+	app.post('/logout', function(req, res){
+		console.log( "post /logout:");
+		res.clearCookie('user');
+		res.clearCookie('pass');
+		req.session.destroy(function(e){ res.status(200).send('ok'); });
+	});
+	
+	
+	app.get('/home', function(req, res) {
+		if (req.session.user == null){
+	// if user is not logged-in redirect back to login page //
+			res.redirect('/');
+		}	else{
+			res.render('home', {
+				title : 'Control Panel',
+				countries : CT,
+				udata : req.session.user
+			});
+		}
+	});
+	
+	app.post('/home', function(req, res){
+		if (req.session.user == null){
+			res.redirect('/');
+		}	else{
+			AM.updateAccount({
+				id		: req.session.user._id,
+				name	: req.body['name'],
+				email	: req.body['email'],
+				pass	: req.body['pass'],
+				country	: req.body['country']
+			}, function(e, o){
+				if (e){
+					res.status(400).send('error-updating-account');
+				}	else{
+					req.session.user = o;
+					// update the user's login cookies if they exists //
+					if (req.cookies.user != undefined && req.cookies.pass != undefined){
+						res.cookie('user', o.user, { maxAge: 900000 });
+						res.cookie('pass', o.pass, { maxAge: 900000 });	
 					}
-				});
-			}	else{
-				res.status(400).send('email-not-found');
-			}
-		});
-	});
-
-	// reset password
-	app.get('/reset-password', function(req, res) {
-		var email = req.query["e"];
-		var passH = req.query["p"];
-		AM.validateResetLink(email, passH, function(e){
-			if (e != 'ok'){
-				res.redirect('/');
-			} else{
-	// save the user's email in a session instead of sending to the client //
-				req.session.reset = { email:email, passHash:passH };
-				res.render('reset', { title : 'Reset Password' });
-			}
-		});
+					res.status(200).send('ok');
+				}
+			});
+		}
 	});
 	
-	app.post('/reset-password', function(req, res) {
-		var nPass = req.body['pass'];
-	// retrieve the user's email from the session to lookup their account and reset password //
-		var email = req.session.reset.email;
-	// destory the session immediately after retrieving the stored email //
-		req.session.destroy();
-		AM.updatePassword(email, nPass, function(e, o){
-			if (o){
+	app.get('/signup', function(req, res) {
+		res.render('signup', {  title: 'Signup', countries : CT });
+	});
+	
+	app.post('/signup', function(req, res){
+		AM.addNewAccount({
+			name 	: req.body['name'],
+			email 	: req.body['email'],
+			user 	: req.body['user'],
+			pass	: req.body['pass'],
+			country : req.body['country']
+		}, function(e){
+			if (e){
+				res.status(400).send(e);
+			}	else{
 				res.status(200).send('ok');
-			}	else{
-				res.status(400).send('unable to update password');
 			}
 		});
 	});
 	
-// view & delete accounts //
+	// view & delete accounts //
 	app.get( '/test/workout', function(req,res){
 		workout.findAll( function( e, workouts){
 			res.render( 'workouts', { title : 'Workout List', list : workouts});
@@ -221,5 +271,4 @@ module.exports = function(app) {
 		// res.render('404', { title: 'Page Not Found'}); 
 		res.status(404).send( [{message:"Page Not Found"}]);
 	});
-
 };
