147700bd74c0085027ccc0ba0ac65d3aae37dabd
nik <nik_rb@yahoo.com>
2016-04-29 03:23:04 +0000

    add workout save

--- app/server/modules/workout.js
+++ app/server/modules/workout.js
@@ -22,8 +22,8 @@ exports.findAll = function( callback){
 		} else {
 		    var promises = [];
 		    for( var i=0; i<res.length; i++){
-		        var workout = res[i];
 		        var p = new Promise( function( resolve, reject){
+		            var workout = res[i];
                     accounts.find( { _id : ObjectId( workout.owner)}).limit(1).next( function( acc_err, workout_owner){
                         if( acc_err){
                             workout.owner_name = "not found";
@@ -44,3 +44,48 @@ exports.findAll = function( callback){
 	});
 };
 
+exports.saveAll = function( req, res){
+    var user_id = req.session.user._id;
+    console.log( "Workout.saveAll owner:", user_id);
+    var workout_list = req.body.map( function( obj){
+        obj.owner = user_id;
+        return obj;
+    });
+    var promises = [];
+    for( var i =0; i<workout_list.length; i++){
+        var p = new Promise( function( resolve, reject){
+            var workout = workout_list[i];
+            console.log( "creating promise for workout:", workout);
+            if( workout.mid.length){
+                console.log( "replacing object with mid");
+                workouts.findOneAndReplace( { _id : ObjectId( workout.mid)},
+                        workout, { projection: { ios_id:1, _id:1}, 
+                                returnOriginal:false, 
+                                upsert:true}, 
+                    function( err, result){
+                        console.log( "dealing with workout:", result.value);
+                    if( err){
+                        resolve( { error:true, message:err});
+                    } else {
+                        resolve( result.value);
+                    }
+                });
+            } else {
+                console.log( "inserting object without mid");
+                workouts.insertOne( workout, [], function( err, result){
+                    if( err){
+                        resolve( { error:true, message:err});
+                    } else {
+                        resolve( { ios_id : result.ops[0].ios_id, 
+                                    _id:result.ops[0]._id});
+                    }
+                });
+            }
+        });
+        promises.push( p);
+    }
+	Promise.all( promises).then( function( results){
+	    console.log( "@Workout.saveAll results:", results);
+	    res.status(200).send( results);
+	});
+};
