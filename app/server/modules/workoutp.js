fe538478586cb97a2ccd09d9aa6b6e9bfac71de9
nik <nik_rb@yahoo.com>
2016-05-23 00:15:46 +0000

    remove some debug

--- app/server/modules/workoutp.js
+++ app/server/modules/workoutp.js
@@ -17,30 +17,24 @@ MongoClient.connect(url, function(err, db) {
 
 exports.findWorkoutByMidWithDrills = function( workout_mid, owner){
     return new Promise( function( resolve, reject){
-        console.log( "inside promise");
         workouts.find( { _id : ObjectId( workout_mid)})
         .toArray( function( err, workout_one){
-            console.log( "found workouts:", workout_one);
             if( err){
                 reject( err);
             } else {
                 var workout = workout_one[0];
                 var workout_drills = workout.hasDrills.split(",")
                 .map( function( id){
-                    console.log( "drill id:", id);
                     return ObjectId( id);
                 });
                 var owners = [ "system", owner];
-                console.log( "finding drill list:", workout_drills);
-                console.log( "owner list:", owners);
                 drills.find( {_id : { $in : workout_drills} , owner: { $nin : owners} } )
                 .toArray( function( err, unowned_drills){
                     if( err){
-                        console.log( "@@routine.findWorkoutByMidWithDrills failed:", err);
+                        console.log( "@routine.findWorkoutByMidWithDrills failed:", err);
                     } else {
                         workout.drill_data = unowned_drills;
                     }
-                    console.log( "resolve with workout:", workout);
                     resolve( workout);
                 });
             }
